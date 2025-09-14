22dd4001f702bc1bcc80ccda81185b1475de1cfa
Ryan Luong <lumintriet@gmail.com>
2025-09-14 19:50:57 +0700

    TanStack Router setup.

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["dist/", "convex/_generated/"]),
+  globalIgnores(["dist/", "convex/_generated/", "routeTree.gen.ts"]),
   {
     name: "js/recommended",
     ...js.configs.recommended
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
@@ -5,6 +6,10 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true
+    }),
     react({
       babel: {
         plugins: ["babel-plugin-react-compiler"]
